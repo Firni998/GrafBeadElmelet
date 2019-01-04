eb32d0b7b9e5f290a371ed48b92ff8d054d603b8
Firni <firniglr@gmail.com>
2019-01-04 18:20:31 +0100

    Add project files.

---
+++ GrafBeadElmelet/Csucsok.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrafBeadElmelet
+{
+    class Csucsok
+    {
+        
+        int id;
+        
+        public Csucsok(int id)
+        {
+            this.id = id;
+        }
+
+        public override string ToString()
+        {
+            return this.id.ToString();
+        }
+    }
+}
